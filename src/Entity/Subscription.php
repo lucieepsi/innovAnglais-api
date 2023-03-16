2369f3189fab2b791e1f9b0e38018669e5f00790
thibalt.meurin@ThiBalt <thibalt.meurin@laposte.net>
2023-03-16 17:20:14 +0100

    fix

--- src/Entity/Subscription.php
+++ src/Entity/Subscription.php
@@ -2,12 +2,13 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SubscriptionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
 
 #[ORM\Entity(repositoryClass: SubscriptionRepository::class)]
 #[ApiResource]
