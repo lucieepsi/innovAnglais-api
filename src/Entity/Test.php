ecae1611714c341553dd2b321967b804d5626fb1
thibalt.meurin@ThiBalt <thibalt.meurin@laposte.net>
2023-03-16 14:55:53 +0100

    added authintication

--- src/Entity/Test.php
+++ src/Entity/Test.php
@@ -7,10 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 
 #[ORM\Entity(repositoryClass: TestRepository::class)]
-#[ApiResource]
+#[ApiResource(normalizationContext:['groups' => ['read']])]
 #[ORM\Table(name:"Tests")]
 class Test
 {
